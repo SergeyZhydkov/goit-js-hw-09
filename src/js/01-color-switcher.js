51313a5757fced1eafee0773af1e9d783328c96e
SergeyZhydkov <134008114+SergeyZhydkov@users.noreply.github.com>
2023-10-17 02:21:21 +0300

    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const selector = {
+  btnStart: document.querySelector('.btn-start'),
+  btnStop: document.querySelector('.btn-stop'),
+  body: document.querySelector('body'),
+};
 
+let colorPage;
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+selector.btnStart.addEventListener('click', onStart);
+function onStart() {
+  colorPage = setInterval(() => {
+    selector.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+
+  selector.btnStart.disabled = true;
+}
+selector.btnStop.addEventListener('click', onStop);
+function onStop() {
+  clearInterval(colorPage);
+  selector.btnStart.disabled = false;
+}
